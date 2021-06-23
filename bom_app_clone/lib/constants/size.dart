f9aaf1231873d5ad8aa122a75e1bd4bf3fde7375
Yxxnsu <64016453+dustn0121@users.noreply.github.com>
2021-06-23 15:15:45 +0900

    연수,추가 및 수정

---
+++ bom_app_clone/lib/constants/size.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MediaSize{
+  late Size size;
+}
--- bom_app_clone/lib/pointerSignal.dart
+++ bom_app_clone/lib/pointerSignal.dart
@@ -1,11 +1,3 @@
-import 'package:bom_app_clone/views/charge.dart';
-import 'package:bom_app_clone/views/chat.dart';
-import 'package:bom_app_clone/views/check.dart';
-import 'package:bom_app_clone/views/health.dart';
-import 'package:bom_app_clone/views/home.dart';
-import 'package:bom_app_clone/views/info.dart';
-import 'package:bom_app_clone/views/manager.dart';
-import 'package:bom_app_clone/views/market.dart';
 import 'package:flutter/gestures.dart';
 import 'package:get/get.dart';
 
