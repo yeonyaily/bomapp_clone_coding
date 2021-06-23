54215aaca69d16a957ac1f4a08625f9b662a64d3
yechan <21801073@handong.edu>
2021-06-23 20:55:15 +0900

    yechan 06-23 20:55 manager page

---
+++ bom_app_clone/lib/widget/body_template.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+body_template(String category, String headline, String bodytext, String link) {
+  return Container(
+    child: Row(
+      children: [
+        Column(
+          children: [
+            Text(''),
+          ],
+        )
+      ],
+    ),
+  );
+}
